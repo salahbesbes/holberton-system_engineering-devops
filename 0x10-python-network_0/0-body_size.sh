120fae3fe8bf07eaf0883113ef024a2e708e0169
salahbesbes <salah_besbes@ymail.com>
2021-04-08 14:36:30 +0200

    het header and print content-legth

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# using curl command to pink an URL and print the content-length of the
-# response
-
-URL=$1
-response=$(curl -sI "$URL")
-res=$( echo "$response" | grep 'Content-Length' | cut  -d ':' -f '2' | cut -d ' ' -f '2')
-echo "$res"
-
+# using curl command to pink an URL and print the content-length of the response
+echo $"$(curl -sI "$1" | grep 'Content-Length' | cut  -d ':' -f '2' | cut -d ' ' -f '2')"
