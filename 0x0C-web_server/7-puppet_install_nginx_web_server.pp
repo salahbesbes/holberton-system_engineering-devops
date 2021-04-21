c5d777c2eb6486a62608e3518f38fc63cf57c2db
salahbesbes <salah_besbes@ymail.com>
2021-04-22 01:32:05 +0200

    puppet config

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -18,12 +18,5 @@ file { '/usr/www/html/index.html':
   mode    => '0644',
   content => 'Holberton School';
 }
-->
-file_line {'redirect':
-  ensure => present,
-  after  => 'listen \[::\]:80',
-  path   => '/etc/nginx/sites-available/default',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
-}
 
 
