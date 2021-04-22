3baf9614e48c16f753b9549f85e398fbf2058d8b
salahbesbes <salah_besbes@ymail.com>
2021-04-22 02:23:42 +0200

    puppet config

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,30 @@
 # automatic install and config nginx
 exec { 'update':
-  command => '/usr/bin/apt-get update',
-}
-->
+  command => 'apt-get update',
+  path    => '/usr/bin/:/bin/'
+}->
 package { 'nginx':
-  ensure   => present,
-  provider => apt;
-} ->
+  ensure   => present, # must be present not any else
+  provider => apt
+}->
 file { '/etc/nginx/sites-enabled/default':
   ensure => link,
-  target => '/etc/nginx/sites-available/default';
+  target => '/etc/nginx/sites-available/default',
 }->
-file { '/usr/www/html/index.html':
+file { '/var/www/html/index.html':
   ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
   mode    => '0644',
-  content => 'Holberton School';
+  content => 'Holberton School',
+}->
+file_line {'add_redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => "location /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ;\n\t\t}",
+  notify => Service['nginx']
+}
+service { 'nginx':
+  ensure     => running,
 }
 
 
