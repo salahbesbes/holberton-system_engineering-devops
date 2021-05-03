bfc8e843a46f3a8c766b9acdd52ed7e8dee76658
salahbesbes <salah_besbes@ymail.com>
2021-05-03 17:14:30 +0200

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,13 +14,11 @@ if __name__ == "__main__":
                                      delimiter=',',
                                      quotechar='"',
                                      quoting=csv.QUOTE_ALL)
-        response = requests.request(
-                'get',
+        response = requests.get(
                 'https://jsonplaceholder.typicode.com/users/{}/todos'
                 .format(emp_id))
-        res = requests.request('get',
-                               'https://jsonplaceholder.typicode.com/users/{}'
-                               .format(emp_id))  # list of dicts
+        res = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                           .format(emp_id))  # list of dicts
         todo_list = json.loads(response.text)
         employee_name = json.loads(res.text).get('name')
 
