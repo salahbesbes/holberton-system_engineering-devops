04d60c3cfc5d23e3e38674ee07c632a21b8a94b4
salahbesbes <salah_besbes@ymail.com>
2021-05-03 05:23:28 +0200

    first commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" requests module """
+import json
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        exit(1)
+    emp_id = argv[1]
+    response = requests.request(
+        'get',
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(emp_id))
+    res = requests.request('get',
+                           'https://jsonplaceholder.typicode.com/users/{}'
+                           .format(emp_id))  # list of dicts
+    todo_list = json.loads(response.text)
+    employee_name = json.loads(res.text).get('name')
+    total_todos = len(todo_list)
+    list_completed_todos = [el for el in todo_list if el.get('completed')]
+    completed_todos = len(list_completed_todos)
+    print('Employee {} is done with tasks({}/{}):'.format(employee_name,
+                                                          completed_todos,
+                                                          total_todos))
+
+    for todo in list_completed_todos:
+        print('\t {}'.format(todo.get('title')))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" requests module """
+import json
+from sys import argv
+import requests
+import csv
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        exit(1)
+    emp_id = argv[1]
+    with open('{}.csv'.format(emp_id), mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        response = requests.request(
+            'get',
+            'https://jsonplaceholder.typicode.com/users/{}/todos'.format(emp_id))
+        res = requests.request('get',
+                               'https://jsonplaceholder.typicode.com/users/{}'
+                               .format(emp_id))  # list of dicts
+        todo_list = json.loads(response.text)
+        employee_name = json.loads(res.text).get('name')
+
+        for todo in todo_list:
+            row_to_write = [emp_id, employee_name,
+                            todo.get('completed'),
+                            todo.get('title')]
+            employee_writer.writerow(row_to_write)
