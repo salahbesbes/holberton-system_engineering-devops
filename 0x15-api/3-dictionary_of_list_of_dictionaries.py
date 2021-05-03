33c3c6a1f252e2b5c7c6684623ab6d9afcca7266
salahbesbes <salah_besbes@ymail.com>
2021-05-03 18:01:22 +0200

    update

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" requests module """
+import json
+import requests
+
+if __name__ == "__main__":
+
+    with open('todo_all_employees.json', mode='w') as employee_file:
+        all_employee = requests.get(
+            'https://jsonplaceholder.typicode.com/users/').json()
+        list_id = [emp.get('id') for emp in all_employee]
+
+        object_to_dump = {}
+        for user in all_employee:
+            user_id = user.get('id')
+            todo_list = requests.get(
+                'https://jsonplaceholder.typicode.com/users/{}/todos'
+                .format(user_id)).json()
+            array_todo = []
+            for todo in todo_list:
+                tmp = {'task': todo.get('title'),
+                       'completed': todo.get('completed'),
+                       'username': user.get('username')}
+                array_todo.append(tmp)
+            object_to_dump.update({user_id: array_todo})
+
+        json.dump(object_to_dump, employee_file, indent=2)
