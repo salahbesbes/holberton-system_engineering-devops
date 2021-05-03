9d272a503486b67445fbfe217694499e2f18c2d6
salahbesbes <salah_besbes@ymail.com>
2021-05-03 17:19:09 +0200

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,21 +6,19 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        exit()
+
     emp_id = argv[1]
     with open('{}.csv'.format(emp_id), mode='w') as employee_file:
         employee_writer = csv.writer(employee_file,
                                      delimiter=',',
                                      quotechar='"',
                                      quoting=csv.QUOTE_ALL)
-        response = requests.get(
-                'https://jsonplaceholder.typicode.com/users/{}/todos'
-                .format(emp_id))
-        res = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                           .format(emp_id))  # list of dicts
-        todo_list = json.loads(response.text)
-        employee_name = json.loads(res.text).get('name')
+        todo_list = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}/todos'
+            .format(emp_id)).json()
+        employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                                .format(emp_id)).json()
+        employee_name = employee.get('name')
 
         for todo in todo_list:
             row_to_write = [emp_id, employee_name,
