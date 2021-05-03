786b713cc89a9e476ef263c4ffc9d8d55d2a4a4e
salahbesbes <salah_besbes@ymail.com>
2021-05-03 05:21:31 +0200

    update

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# using curl command to pink an URL and print the content-length of the response
-echo $"$(curl -sI "$1" | grep 'Content-Length' | cut  -d ':' -f '2' | cut -d ' ' -f '2')"
--- 0x10-python-network_0/README.md
+++
@@ -1,1 +0,0 @@
-# network with Python
