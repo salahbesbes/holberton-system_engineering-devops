beffeb82c6fba22118a0c17e9375bba7d81a0faf
salahbesbes <salah_besbes@ymail.com>
2021-05-18 22:29:44 +0200

    use recursive

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,34 +3,30 @@
 import requests
 
 
-def get_title(obj, hot_list):
-    if type(obj) is list:
-        for el in obj:
-            if type(el) is dict:
-                get_title(el, hot_list)
-    elif type(obj) is dict:
-        for key, val in obj.items():
-            if key == 'title':
-                hot_list.append(obj.get(key))
-            elif type(val) is dict:
-                get_title(val, hot_list)
-            elif type(val) is list:
-                get_title(val, hot_list)
-    return hot_list
-
-
-def recurse(subreddit, hot_list=[]):
+def get_title(subreddit, hot_list, after=True):
     url = "https://www.reddit.com/r/{}/{post_type}.json".format(
         subreddit, post_type='hot')
-
+    params = {'after': after,
+              'limit': 100}
     response = requests.get(url,
                             headers={"User-agent": "Any thing"},
-                            allow_redirects=False)
+                            allow_redirects=False,
+                            params=params)
 
     try:
         data = response.json()
+        list_items = data['data']['children']
+        after = data.get('data').get('after')
 
-        return get_title(data, hot_list)
+        if after:
+            for item in list_items:
+                hot_list.append(item.get('title'))
+            get_title(subreddit, hot_list, after)
 
+        return hot_list
     except Exception:
         print(None)
+
+
+def recurse(subreddit, hot_list=[]):
+    return get_title(subreddit, hot_list)
