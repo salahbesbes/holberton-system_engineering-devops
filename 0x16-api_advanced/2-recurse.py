becc813185b2e72012203b3738173221de42f71e
salahbesbes <salah_besbes@ymail.com>
2021-05-18 22:38:34 +0200

    use recursive

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ def get_title(subreddit, hot_list, after=True):
     url = "https://www.reddit.com/r/{}/{post_type}.json".format(
         subreddit, post_type='hot')
     params = {'after': after,
-              'limit': 100}
+              'limit': 500}
     response = requests.get(url,
                             headers={"User-agent": "Any thing"},
                             allow_redirects=False,
@@ -16,11 +16,11 @@ def get_title(subreddit, hot_list, after=True):
     try:
         data = response.json()
         list_items = data['data']['children']
+        # after is an ttribut that tell us that we can found more data
         after = data.get('data').get('after')
-
+        for item in list_items:
+            hot_list.append(item.get('title'))
         if after:
-            for item in list_items:
-                hot_list.append(item.get('title'))
             get_title(subreddit, hot_list, after)
 
         return hot_list
